a9d37952270ffca000ec1739949176ce482c9cef
Duque <jduquebl@alumnos.unex.es>
2018-10-20 18:54:07 +0200

    Clase Client get/set

--- Client.java
+++ Client.java
@@ -1,4 +1,4 @@
-
+import java.util.Collection;
 /**
  * Write a description of class Client here.
  *
@@ -8,26 +8,113 @@
 public class Client
 {
     // instance variables - replace the example below with your own
-    private int x;
+
+    private String name;
+    private int id;
+    private int age;
+    private String actualLocation;
 
     /**
      * Constructor for objects of class Client
      */
     public Client()
     {
-        // initialise instance variables
-        x = 0;
+        this.name="";
+        this.id=0;
+        this.age=0;
+        this.actualLocation="";
+
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Constructor Parametrized for objects of class Client
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public Client(String name, int id, int age, String actualLocation){
+        this.name=name;
+        this.id=id;
+        this.age=age;
+        this.actualLocation=actualLocation;
+
     }
+
+    /**
+     * Get name
+     *
+     * @param  
+     * @return    name
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * Get Id
+     *
+     * @param  
+     * @return    id
+     */ 
+    public int getId(){
+        return this.id;
+    }
+
+    /**
+     * Get Age
+     *
+     * @param  
+     * @return    age
+     */
+    public int getAge(){
+        return this.age;
+    }
+
+    /**
+     * Get ActualLocation
+     *
+     * @param  
+     * @return    actualLocation
+     */
+    public String getActualLocation(){
+        return this.actualLocation;
+    }
+
+    /**
+     * Set Name
+     *
+     * @param  name
+     * @return    
+     */
+    public void setName(String name){
+        this.name=name;
+    }
+
+    /**
+     * Set Id
+     *
+     * @param  id
+     * @return    
+     */ 
+    public void setId(int id){
+        this.id=id;
+    }
+
+    /**
+     * Set Age
+     *
+     * @param  age
+     * @return    
+     */
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    /**
+     * Set ActualLocation
+     *
+     * @param   actualLocation
+     * @return    
+     */
+    public void setActualLocation(String actualLocation){
+        this.actualLocation=actualLocation;
+    }
+    
 }
