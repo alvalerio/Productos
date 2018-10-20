6cdacef09a848ec3d8849cd83efba6a2699dc88c
Alvalerio <alvalerio@alumnos.unex.es>
2018-10-20 22:09:20 +0200

    Comment Class Added

---
+++ Comments.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Comments here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Comments
+{
+    private String text; 
+    private String nameClient; 
+    private Integer points;
+
+    /**
+     * Constructor for objects of class Comments
+     */
+    public Comments()
+    {
+        this.text = ""; 
+        this.nameClient = ""; 
+        this.points = 0; 
+       
+    }
+/**
+     * Parametricsize Constructor for objects of class Comments
+     */
+    public Comments(String text, String nameClient, Integer points)
+    {
+        this.text = text; 
+        this.nameClient = nameClient; 
+        this.points = points; 
+       
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod(int y)
+    {
+        // put your code here
+       
+    }
+}
