f71bd4430bafb6326780624d64d6457d04a0dbd5
Alvalerio <alvalerio@alumnos.unex.es>
2018-10-29 12:28:41 +0100

    PRE/POST Class Comments

--- Comments.java
+++ Comments.java
@@ -24,6 +24,9 @@ public class Comments
 
     /**
      * Parametricsize Constructor for objects of class Comments
+     * @param String text The body text of the comment. 
+     * @param nameClient The name of the Client who want to comment.
+     * @param Integer points The number of points to give to the product. 
      */
     public Comments(String text, String nameClient, Integer points)
     {
@@ -40,7 +43,11 @@ public class Comments
         }
 
     }
-    
+    /**
+     * Show the details of a Comment. This include the name of the CLient who has commented, the body text of the Comment and the value of points the Client has
+     * give to the Product. 
+     *  
+     */
     public void ShowComment(){
      
         System.out.println("Client: " + this.nameClient); 
