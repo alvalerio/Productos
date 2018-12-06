93b9ce18f4860356f12286221b10acd636f26829
Duque <jduquebl@alumnos.unex.es>
2018-12-06 20:27:27 +0100

    Herencia producto y no comentar la alimentacion

---
+++ LeisureProduct.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class LeisureProduct here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LeisureProduct extends Product
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class LeisureProduct
+     */
+    public LeisureProduct()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
